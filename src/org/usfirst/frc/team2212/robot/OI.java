732574f936b4ec9d528f3156119aa4ff4077108f
Tomer Sitton <tomer.sitton1@gmail.com>
2017-01-11 12:38:05 +0200

    deleted unneeded imports

--- src/org/usfirst/frc/team2212/robot/OI.java
+++ src/org/usfirst/frc/team2212/robot/OI.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc.team2212.robot;
 
-import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTankWithPID;
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
@@ -13,9 +10,9 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI {
 	Joystick joy;
 	JoystickButton forward;
+
 	public OI() {
-		joy= new Joystick(0);
-		forward= new JoystickButton(joy, 2);
+		joy = new Joystick(0);
+		forward = new JoystickButton(joy, 2);
 	}
 }
-
