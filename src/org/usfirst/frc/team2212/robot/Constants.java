6bd173d3e0c8068073ffbbc5b7fd4019aab42bbe
Spikes 2212 <spikes.no2212@gmail.com>
2017-01-13 15:01:04 +0200

    Fixed MaxSpeed and getTurnSpeed

--- src/org/usfirst/frc/team2212/robot/Constants.java
+++ src/org/usfirst/frc/team2212/robot/Constants.java
@@ -1,15 +1,13 @@
 package org.usfirst.frc.team2212.robot;
 
 public class Constants {
-	public static final double MAX_SPEED = 0.5;
-	public static final double CAMERA_WIDTH=640;
-	public static double setpoint=1;
-	public static double KP,KI,KD;
-	
+	public static final double MAX_SPEED = 1;
+	public static final double CAMERA_WIDTH = 640;
+	public static double setpoint = 1;
+	public static double KP, KI, KD;
+
 	public static double getTurnSpeed(double position) {
-		if(position>0)
-			return MAX_SPEED*position;
-		return -MAX_SPEED*position;
+		return MAX_SPEED * (position - 0.5);
 	}
 
 }
