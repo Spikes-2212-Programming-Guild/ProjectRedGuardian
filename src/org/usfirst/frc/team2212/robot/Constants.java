197c7188db5ac994d3dba3e1610714a5e29d39dc
Spikes 2212 <spikes.no2212@gmail.com>
2017-01-13 13:45:15 +0200

    fixed camera width

--- src/org/usfirst/frc/team2212/robot/Constants.java
+++ src/org/usfirst/frc/team2212/robot/Constants.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team2212.robot;
 
 public class Constants {
 	public static final double MAX_SPEED = 0.5;
-	public static final double CAMERA_WIDTH=320;
+	public static final double CAMERA_WIDTH=640;
 	public static double setpoint=1;
 	public static double KP,KI,KD;
 	
