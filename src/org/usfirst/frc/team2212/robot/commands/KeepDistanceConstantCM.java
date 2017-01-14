62bf25fce652793f86a0137cab2a2824f1346cea
Spikes 2212 <spikes.no2212@gmail.com>
2017-01-14 14:01:59 +0200

    Trying to fix commands

--- src/org/usfirst/frc/team2212/robot/commands/KeepDistanceConstantCM.java
+++ src/org/usfirst/frc/team2212/robot/commands/KeepDistanceConstantCM.java
@@ -9,20 +9,21 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class KeepDistanceConstantCM extends CommandGroup{
 	private static final int WIDTH_PIXEL=640;
-	private static final double OBJECT_WIDTH_CM=10;
+	private static final double OBJECT_WIDTH_CM=68.3;
 	private static final double VIEW_ANGLE=50;
+	private static final double DISTANCE_TO_VELCOTY_FACTOR=0.2;
 	private static double widthCentemters,distance;
 	public KeepDistanceConstantCM(Drivetrain drivetrain,NetworkTable networkTables,double wantedDistance){
 		widthCentemters=(OBJECT_WIDTH_CM*WIDTH_PIXEL)/networkTables.getNumber("width", 0);
 		distance=(0.5*widthCentemters)/Math.tan(VIEW_ANGLE/2);
 		if(distance>wantedDistance){
-			addSequential(new DriveTank(drivetrain, wantedDistance-distance, wantedDistance-distance));
+			addSequential(new DriveTank(drivetrain, (wantedDistance-distance)*DISTANCE_TO_VELCOTY_FACTOR,DISTANCE_TO_VELCOTY_FACTOR*(wantedDistance-distance)));
 		}
 		if(distance<wantedDistance){
-			addSequential(new DriveTank(drivetrain, -wantedDistance+distance, -wantedDistance+distance));
+			addSequential(new DriveTank(drivetrain, (-wantedDistance+distance)*DISTANCE_TO_VELCOTY_FACTOR, DISTANCE_TO_VELCOTY_FACTOR*(-wantedDistance+distance)));
 		}
-		if(distance==wantedDistance||distance==0){
-			addSequential(new DriveTank(drivetrain, 0, 0));
+		if(distance==wantedDistance){
+			addSequential(new DriveTank(drivetrain, 0.0, 0.0));
 		}
 	}
 }
--- src/org/usfirst/frc/team2212/robot/commands/KeepDistanceConstantPixel.java
+++ src/org/usfirst/frc/team2212/robot/commands/KeepDistanceConstantPixel.java
@@ -8,12 +8,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class KeepDistanceConstantPixel extends CommandGroup{
+	private static final double DISTANCE_TO_VELCOTY_FACTOR=0.2;
 	public KeepDistanceConstantPixel(Drivetrain dt, NetworkTable nt, double widthPixel) {
 			if (nt.getNumber("width",0) > widthPixel) {
-				addSequential(new DriveTank(dt, widthPixel-nt.getNumber("width",0), widthPixel-nt.getNumber("width",0)));
+				addSequential(new DriveTank(dt, DISTANCE_TO_VELCOTY_FACTOR*(widthPixel-nt.getNumber("width",0)), -DISTANCE_TO_VELCOTY_FACTOR*(widthPixel-nt.getNumber("width",0))));
 			}
 			else if (nt.getNumber("width",0) < widthPixel) {
-				addSequential(new DriveTank(dt, -widthPixel+nt.getNumber("width",0), -widthPixel+nt.getNumber("width",0)));
+				addSequential(new DriveTank(dt, DISTANCE_TO_VELCOTY_FACTOR*(-widthPixel+nt.getNumber("width",0)), -DISTANCE_TO_VELCOTY_FACTOR*(-widthPixel+nt.getNumber("width",0))));
 			}else if(nt.getNumber("width",0)== 0 || nt.getNumber("width",0)==widthPixel){
 				addSequential(new DriveTank(dt, 0, 0));
 			}
