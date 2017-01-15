9c5b36d0faab2db2b3e53a0457db67f9401502d5
roeeiit1 <roeeiit1@gmail.com>
2017-01-15 18:25:19 +0200

    Added suppliers of tolerance KP KD KI

--- src/org/usfirst/frc/team2212/robot/Constants.java
+++ src/org/usfirst/frc/team2212/robot/Constants.java
@@ -1,11 +1,18 @@
 package org.usfirst.frc.team2212.robot;
 
+import java.util.function.Supplier;
+
+import com.spikes2212.dashboard.ConstantHandler;
+
 public class Constants {
 	public static final double MAX_SPEED = 1;
 	public static final double CAMERA_WIDTH = 640;
 	public static double setpoint = 1;
 	public static double KP, KI, KD;
-
+	public static final Supplier<Double> KPS = ConstantHandler.addConstantDouble("KP", 0.0);
+	public static final Supplier<Double> KIS = ConstantHandler.addConstantDouble("KI", 0.0);
+	public static final Supplier<Double> KDS = ConstantHandler.addConstantDouble("KD", 0.0);
+	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("Tolerance", 0.0);
 	public static double getTurnSpeed(double position) {
 		return MAX_SPEED * (position - 0.5);
 	}
