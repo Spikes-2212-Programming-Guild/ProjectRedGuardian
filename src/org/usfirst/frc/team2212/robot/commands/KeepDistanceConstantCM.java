27128f3e42882033113e01d97766f02512c7bf2a
roeeiit1 <roeeiit1@gmail.com>
2017-01-15 14:33:47 +0200

    Fixed numbers

--- src/org/usfirst/frc/team2212/robot/commands/KeepDistanceConstantCM.java
+++ src/org/usfirst/frc/team2212/robot/commands/KeepDistanceConstantCM.java
@@ -15,14 +15,18 @@ public class KeepDistanceConstantCM extends CommandGroup{
 	private static double widthCentemters,distance;
 	public KeepDistanceConstantCM(Drivetrain drivetrain,NetworkTable networkTables,double wantedDistance){
 		widthCentemters=(OBJECT_WIDTH_CM*WIDTH_PIXEL)/networkTables.getNumber("width", 0);
-		distance=(0.5*widthCentemters)/Math.tan(VIEW_ANGLE/2);
-		if(distance>wantedDistance){
-			addSequential(new DriveTank(drivetrain, (wantedDistance-distance)*DISTANCE_TO_VELCOTY_FACTOR,DISTANCE_TO_VELCOTY_FACTOR*(wantedDistance-distance)));
+		distance=(0.5 * widthCentemters) / Math.tan(VIEW_ANGLE / 2);
+		if(distance > wantedDistance){
+			addSequential(new DriveTank(drivetrain,
+					(-1) * (wantedDistance - distance) * DISTANCE_TO_VELCOTY_FACTOR,
+					DISTANCE_TO_VELCOTY_FACTOR * (wantedDistance - distance)));
 		}
-		if(distance<wantedDistance){
-			addSequential(new DriveTank(drivetrain, (-wantedDistance+distance)*DISTANCE_TO_VELCOTY_FACTOR, DISTANCE_TO_VELCOTY_FACTOR*(-wantedDistance+distance)));
+		else if(distance < wantedDistance){
+			addSequential(new DriveTank(drivetrain,
+					(-1) * (-wantedDistance + distance) * DISTANCE_TO_VELCOTY_FACTOR,
+					DISTANCE_TO_VELCOTY_FACTOR * (-wantedDistance + distance)));
 		}
-		if(distance==wantedDistance){
+		else {
 			addSequential(new DriveTank(drivetrain, 0.0, 0.0));
 		}
 	}
